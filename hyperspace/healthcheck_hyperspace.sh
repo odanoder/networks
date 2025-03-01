4a6eb25933a09776a94faa314bebadd55bfacb80
OdaNoder <143608461+odanoder@users.noreply.github.com>
2025-03-01 19:24:42 +0200

    Update healthcheck_hyperspace.sh

--- hyperspace/healthcheck_hyperspace.sh
+++ hyperspace/healthcheck_hyperspace.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
+# Путь к файлу, который будет создан
 FILE_PATH="/healthcheck_hyperspace.sh"
 
-SCRIPT_CONTENT='#!/bin/bash
+# Код, который будет помещен в файл (используем двойные кавычки и экранируем специальные символы)
+SCRIPT_CONTENT="#!/bin/bash
 
-# Проверяем, существует ли сессия tmux с именем "hyperspace"
+# Проверяем, существует ли сессия tmux с именем \"hyperspace\"
 if tmux has-session -t hyperspace 2>/dev/null; then
-    echo "Сессия hyperspace уже запущена. Завершение работы скрипта."
+    echo \"Сессия hyperspace уже запущена. Завершение работы скрипта.\"
     exit 0
 else
     # Если сессия не запущена, создаем новую сессию и запускаем команду внутри неё
-    echo "Сессия hyperspace не найдена. Создание новой сессии..."
+    echo \"Сессия hyperspace не найдена. Создание новой сессии...\"
     tmux new-session -d -s hyperspace
-    tmux send-keys -t hyperspace "aios-cli start --connect" C-m
-    echo "Команда \'aios-cli start --connect\' успешно запущена в сессии hyperspace."
+    tmux send-keys -t hyperspace \"aios-cli start --connect\" C-m
+    echo \"Команда \\\"aios-cli start --connect\\\" успешно запущена в сессии hyperspace.\"
 fi
-'
+"
 
 # Создаем файл в корневой директории и записываем в него содержимое
 echo "$SCRIPT_CONTENT" | sudo tee $FILE_PATH > /dev/null
