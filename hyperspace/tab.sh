6c4fa3ade655a0b2452bcf5bb391f525e3e3cc3f
OdaNoder <143608461+odanoder@users.noreply.github.com>
2025-03-01 20:09:00 +0200

    Update tab.sh

--- hyperspace/tab.sh
+++ hyperspace/tab.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-# Имя файла, который нужно найти
 TARGET_FILE="healthcheck_hyperspace.sh"
-
-# Поиск файла в системе
 FILE_PATH=$(find / -name "$TARGET_FILE" 2>/dev/null)
 
-# Проверяем, найден ли файл
 if [ -z "$FILE_PATH" ]; then
     echo "Ошибка: Файл '$TARGET_FILE' не найден."
     exit 1
@@ -14,7 +10,6 @@ else
     echo "Файл '$TARGET_FILE' найден по пути: $FILE_PATH"
 fi
 
-# Проверяем, существует ли уже такое задание в cron
 if crontab -l | grep -q "$FILE_PATH"; then
     echo "Задание для '$FILE_PATH' уже существует в cron."
 else
